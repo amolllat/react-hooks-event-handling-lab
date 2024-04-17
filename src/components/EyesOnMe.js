ee7795109683f82f37c8490aa59f2dd370f0c75e
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-04-17 15:51:53 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+
+export default function EyesOnMe () {
+    const focus = () => {
+        console.log('Good!');
+    };
+    const blur = () => {
+        console.log('Hey! Eyes on me!');
+    }; 
+    return (
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleInput = () => {
+        console.log("Entering password...");
+    };
+    
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleInput}></input>
+        </div>
     )
 }
 
